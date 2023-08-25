64bd4328708efedb43df67aa5eb0021bd505a0ec
Sujayweb3master <122873224+Sujayweb3master@users.noreply.github.com>
2023-08-25 19:34:15 +0530

    Add some lines of code

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from library import greet
-
 def main():
-    greet()
+    book, author, release_year, goodreads_rating = 'Dracula', 'Bram Stoker', 1897, 4.01
 
+    # print(book + ' is a novel by ' + author + ', published in ' + str(release_year) + '. It has a rating of ' + str(goodreads_rating) + ' on goodreads.')
+    print(f'{book} is a novel by {author}, published in {release_year}. It has a rating of {goodreads_rating}')
 
 if __name__ == '__main__':
     main()
-
-# Hello, World!
